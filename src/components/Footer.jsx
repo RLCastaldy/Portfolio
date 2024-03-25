e3b4df64d34b21b9f570928e94514e94e5538558
RLCastaldy <rlcastaldy@gmail.com>
2024-03-25 15:54:03 -0500

    fixed spacing for footerLinks

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,18 +2,19 @@ import '../styles/Footer.css';
 
 const styles = {
   footer: {
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    display: 'flex',
+    justifyContent: 'center',
   },
   footLinks: {
+    fontSize: '20px',
     marginLeft: '25px',
   }
 };
   
   function Footer() {
     return (
-      <header className="footer">
-        <a href="https://github.com/RLCastaldy" style={styles.footer} className='footer'>GitHub</a>
+      <header style={styles.footer} className="footer">
+        <a href="https://github.com/RLCastaldy" style={styles.footLinks} className='footer'>GitHub</a>
         <a href="https://www.linkedin.com/in/richard-castaldy-0b5b732bb/" style={styles.footLinks} className='footLinks'>LinkedIn</a>
     </header>
     );
