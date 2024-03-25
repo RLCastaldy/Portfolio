b9a2d0ef3aef2b0c6c83673605f2fe8fc67f6322
RLCastaldy <rlcastaldy@gmail.com>
2024-03-25 16:44:22 -0500

    more styling fixes and added more to AboutMe text

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,16 +1,12 @@
-import '../styles/Project.css';
-
-const styles = {
-    h1: {
-      fontSize: '30px'
-    }
-  
-  }
+import '../styles/AboutMe.css';
   
   function AboutMe() {
     return (
-      <header className="project">
-      <h1 style={styles.h1}>I am an aspiring developer hoping to break into the professional world of web development. I am eager to get started with a company so I can continue building on the core concepts we learned in bootcamp. I am familiar with working on front end technologies such as HTML, CSS, and JS. I also have some experience working with MySQL, MongoDB, and various NPM packages. I can create a restful API and make calls to it with Node JS. I am capable of creating routes to retrieve, post, and delete data as well as test them in Insomnia.</h1>
+      <header className="AboutMe">
+      <h1>I am an aspiring developer hoping to break into the professional world of web development.
+      I am eager to get started with a company so I can continue building on the core concepts we learned in bootcamp.<br></br> <br></br>
+      I am familiar with working on front end technologies such as HTML, CSS, and JS. I also have some experience working with MySQL, MongoDB, and various NPM packages. I can create a restful API and make calls to it with Node JS. I am capable of creating routes to retrieve, post, and delete data as well as test them in Insomnia.<br></br><br></br>
+      Though these are the primary technologies I have been exposed to, I am not shy about learning and embrace the growth that comes with each new experience.</h1>
     </header>
     );
   }
