df1b19a048e1420011395b264f6bc7c8d0dcfc01
RLCastaldy <rlcastaldy@gmail.com>
2024-03-26 13:55:43 -0500

    fixed issue with resume not being able to be downloaded

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,22 +1,23 @@
+import resumeFile from '../assets/RC.pdf'; // Import the resume file
+
 import '../styles/Resume.css';
+
   
 function Resume() {
-  const resumeFile = '/path/to/your/resume.pdf'; // Update the path to your resume file
-
   return (
     <header className="Resume">
-      <a href={resumeFile} download="YourResume.pdf">Download Resume</a>
+      <a href={resumeFile} download="RC.pdf">Download Resume</a>
       <h2 className="">Technologies:</h2>
-      <li>HTML</li>
-      <li>CSS</li>
-      <li>JS</li>
-      <li>Node</li>
-      <li>MySQL</li>
-      <li>MongoDB</li>
-      <li>Various NPM packages</li>
-      <li>React</li>
-      <li>Express</li>
-      <li>APIs</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JS</li>
+        <li>Node</li>
+        <li>MySQL</li>
+        <li>MongoDB</li>
+        <li>Various NPM packages</li>
+        <li>React</li>
+        <li>Express</li>
+        <li>APIs</li>
     </header>
   );
 }
