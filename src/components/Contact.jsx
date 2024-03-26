6566c40f8c215997e81b5212e8b07e5dbade2cc3
RLCastaldy <rlcastaldy@gmail.com>
2024-03-26 15:09:09 -0500

    added form to contact component, need to style and add helpers for name and message

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,72 @@
+import { useState } from 'react';
 import '../styles/Contact.css';
-  
-  function Contact() {
-    return (
-      <header className="Contact">
-      <h1>Contact</h1>
-    </header>
-    );
-  }
-  
-  export default Contact;
+import { validateEmail } from '../utils/helpers.js';
+
+
+function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'message') {
+      setMessage(value);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setErrorMessage('Email is invalid');
+      return;
+    }
+
+    // Clear the form fields after submission
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+
+  return (
+    <div className="container text-center">
+      <form className="form" onSubmit={handleFormSubmit}>
+        <input
+          value={name}
+          name="name"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Name"
+        />
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+          placeholder="Email"
+        />
+        <textarea
+          value={message}
+          name="message"
+          onChange={handleInputChange}
+          placeholder="Message"
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Contact;
