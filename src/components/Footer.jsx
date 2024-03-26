1928da61dbcd5592ce6c0c3524dcd0ab490f2642
RLCastaldy <rlcastaldy@gmail.com>
2024-03-25 21:12:11 -0500

    added link for Twitter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,7 @@ import '../styles/Footer.css';
       <header className="footer">
         <a href="https://github.com/RLCastaldy" className='footLinks'>GitHub</a>
         <a href="https://www.linkedin.com/in/richard-castaldy-0b5b732bb/" className='footLinks'>LinkedIn</a>
+        <a href="https://twitter.com/RLCastaldy" className='footLinks'>Twitter</a>
     </header>
     );
   }
